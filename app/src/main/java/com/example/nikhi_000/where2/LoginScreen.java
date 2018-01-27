c5b7077fb25fb0afb1313181c84c14f41f9b0e67
Nikhil Raina <nikhilrainathegreat@gmail.com>
2018-01-27 18:50:35 -0500

    Nikhil Check

--- app/src/main/java/com/example/nikhi_000/where2/LoginScreen.java
+++ app/src/main/java/com/example/nikhi_000/where2/LoginScreen.java
@@ -9,5 +9,6 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        //Nikhil Here
     }
 }
