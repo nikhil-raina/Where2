80b501de01e3deaae60bcf04f35f2c7f532eccef
Nikhil Raina <nikhilrainathegreat@gmail.com>
2018-01-28 03:45:04 -0500

    Added the dateDuration Class so that the start date and the End date can be recorded in the same position.

---
+++ app/src/main/java/com/example/nikhi_000/where2/dateDuration.java
@@ -0,0 +1,13 @@
+package com.example.nikhi_000.where2;
+
+import java.io.Serializable;
+
+/**
+ *
+ * Created by nikhi_000 on 1/28/2018.
+ */
+
+public class dateDuration implements Serializable {
+    public String startDate;
+    public String endDate;
+}
