72bffcfaf00ca106e00658404ef12c93f00fe5ab
Nikhil Raina <nikhilrainathegreat@gmail.com>
2018-01-27 21:00:30 -0500

    Connected the "next" button and the "ZipCode" textView with the JAVA class

--- app/src/main/java/com/example/nikhi_000/where2/LoginScreen.java
+++ app/src/main/java/com/example/nikhi_000/where2/LoginScreen.java
@@ -2,18 +2,25 @@ package com.example.nikhi_000.where2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class LoginScreen extends AppCompatActivity {
+
+    //private ImageButton imageButton;
+    private Button nextButton;
+    private TextView zipCodeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
-        //Nikhil Here
-        //Shreya Here
-        //Prachi here
-        //Langston here
-        //Preet here
-        //Koshal here
+
+        //imageButton = (ImageButton) findViewById(R.id.imageButton2);
+        zipCodeView = (TextView) findViewById(R.id.editText);
+        nextButton = (Button) findViewById(R.id.button);
+
+
     }
 }
